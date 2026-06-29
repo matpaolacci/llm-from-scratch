96ff6b591d40f863d62e34345cb904fc42311faa
Mattia Paolacci <71827432+matpaolacci@users.noreply.github.com>
2026-06-29 22:00:55 +0200

    Add casual mask to self attention

--- model/self_attention.py
+++ model/self_attention.py
@@ -6,7 +6,7 @@ class SelfAttention(nn.Module):
     def __init__(self, d_model, d_k, d_v):
         super().__init__()
 
-        # Matrici di proiezione
+        # Projection matrices
         self.W_Q = nn.Linear(d_model, d_k, bias=False)
         self.W_K = nn.Linear(d_model, d_k, bias=False)
         self.W_V = nn.Linear(d_model, d_v, bias=False)
@@ -16,24 +16,27 @@ class SelfAttention(nn.Module):
         x: (batch_size, seq_len, d_model)
         """
 
-        # 1. Proiezioni lineari
+        # Linear projection
         Q = self.W_Q(x)      # (B, N, d_k)
         K = self.W_K(x)      # (B, N, d_k)
         V = self.W_V(x)      # (B, N, d_v)
 
-        # 2. Similarità Query-Key
+        # Query-Key similarity
         scores = torch.matmul(Q, K.transpose(-2, -1))
         # (B, N, N)
 
-        # 3. Scaling
+        # Scaling
         scores = scores / (K.size(-1) ** 0.5)
 
-        # 4. Softmax
-        attention = F.softmax(scores, dim=-1)
-        # (B, N, N)
+        # Casual mask
+        scores = Q @ K.transpose(-2, -1)   # QK^T
+        scores = scores / sqrt(d_k)
 
-        # 5. Somma pesata dei Value
-        output = torch.matmul(attention, V)
+        # Softmax (B, N, N)
+        attention = F.softmax(scores, dim=-1)
+
+        # Somma pesata dei Value
+        output = attention @ V
         # (B, N, d_v)
 
         return output, attention
