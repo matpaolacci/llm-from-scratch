49c6cf24f93cd9a95ea30dfb5b13e8e813a10fb7
Mattia Paolacci <71827432+matpaolacci@users.noreply.github.com>
2026-06-29 22:46:50 +0200

    Add precomputed casual masking

--- model/self_attention.py
+++ model/self_attention.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class SelfAttention(nn.Module):
-    def __init__(self, d_model, d_k, d_v):
+    def __init__(self, d_model, d_k, d_v, max_len):
         super().__init__()
 
         # Projection matrices
@@ -12,32 +12,40 @@ class SelfAttention(nn.Module):
         self.W_K = nn.Linear(d_model, d_k, bias=False)
         self.W_V = nn.Linear(d_model, d_v, bias=False)
 
+        # Precomputed casual mask (more efficent)
+        self.register_buffer(
+            "attention_mask",
+            torch.tril(torch.ones(max_len, max_len)).bool()
+        )
+
     def forward(self, x):
         """
         x: (batch_size, seq_len, d_model)
         """
+        B, N, _ = x.shape
 
         # Linear projection
         Q = self.W_Q(x)      # (B, N, d_k)
         K = self.W_K(x)      # (B, N, d_k)
         V = self.W_V(x)      # (B, N, d_v)
 
-        # Query-Key similarity
-        scores = torch.matmul(Q, K.transpose(-2, -1))
-        # (B, N, N)
+        # Query-Key similarity --> (B, N, N)
+        scores = Q @ K.transpose(-2, -1)
 
         # Scaling
         scores = scores / math.sqrt(K.size(-1))
 
-        # Casual mask
-        scores = Q @ K.transpose(-2, -1)   # QK^T
-        scores = scores / math.sqrt(d_k)
+        # Causal masking: blocks attention to future positions by setting upper-triangular
+        # scores to -inf before softmax, enforcing left-to-right autoregressive attention.
+        scores = scores.masked_fill(
+            ~self.mask[:N, :N],
+            float("-inf")
+        )
 
-        # Softmax (B, N, N)
+        # Softmax --> (B, N, N)
         attention = F.softmax(scores, dim=-1)
 
-        # Somma pesata dei Value
+        # Somma pesata dei Value --> (B, N, d_v)
         output = attention @ V
-        # (B, N, d_v)
 
         return output, attention
